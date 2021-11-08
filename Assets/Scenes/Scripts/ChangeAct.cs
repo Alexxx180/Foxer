08e92204ecf74f380343448f5041d4f652fb6b55
Alexxx180 <alexx120702@gmail.com>
2021-11-08 21:31:48 +0300

    Alpha 1.1 | Episode 1 | Act 2 - In the woods

---
+++ Assets/Scenes/Scripts/ChangeAct.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ChangeAct : MonoBehaviour
+{
+    public GameObject previousAct;
+    public GameObject nextAct;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            nextAct.SetActive(true);
+            previousAct.SetActive(false);
+        }
+    }
+}
