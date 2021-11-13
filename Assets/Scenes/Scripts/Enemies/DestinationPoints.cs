70118ccd1f05050e2c9539dcd9b9f4c7b19a8f08
Alexxx180 <alexx120702@gmail.com>
2021-11-13 23:08:37 +0300

    Alpha 1.68 | Enemy: active frog

---
+++ Assets/Scenes/Scripts/Enemies/DestinationPoints.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DestinationPoints : MonoBehaviour
+{
+    public Transform nextDestination;
+    public EnemyAI enemyAI;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            if (enemyAI.target != nextDestination)
+                enemyAI.target = nextDestination;
+        }
+    }
+}
---
+++ Assets/Scenes/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float bounce = 400f;
+    public int hitPoints = 1;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Rigidbody2D rigidbody2D = collision.GetComponentInParent<Rigidbody2D>();
+            rigidbody2D.AddForce(new Vector2(0f, bounce));
+            hitPoints--;
+            if (hitPoints <= 0)
+                Destroy(gameObject);
+        }
+    }
+}
