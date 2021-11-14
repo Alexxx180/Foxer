1f5524ab6c8835758f7962b1816a9efaeb8a462d
Alexxx180 <alexx120702@gmail.com>
2021-11-14 15:26:23 +0300

    Alpha 1.8 | Frog-boss set

--- Assets/Scenes/Scripts/Enemies/BossHpInfo.cs
+++ Assets/Scenes/Scripts/Enemies/BossHpInfo.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHpInfo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Slider bossHp;
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            bossHp.value--;
+        }
     }
 }
